3f51164d5517bf7d63e92de3305c57ba362cb20d
Ben Shillitoe <bshillitoe@gmail.com>
2020-01-15 20:59:51 +0000

    Ongoing

--- helloworld.py
+++ helloworld.py
@@ -44,4 +44,34 @@ print(x,a)
 
 print (type(x))
 
+#Python will auto detec type of variables and numbers
 
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Conveting numbers to other types, although complex numbers can't be converted
+x = 1 # int
+y = 2.8 # float
+z = 1j # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c)) 
